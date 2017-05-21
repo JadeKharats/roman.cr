ecfe2d5f2c4ea1f4dbabb09a39d0d3ed4a6b594a
Jade D. Kharats <jade.kharats@gmail.com>
2017-05-21 14:03:47 +0200

    Add code for convert 5 to V

--- src/roman.cr
+++ src/roman.cr
@@ -3,7 +3,17 @@ require "./roman/*"
 module Roman
   class Convert
     def arabean(n)
-      return "I"*n
+      roman = ""
+      while n > 0
+        if n >= 5
+          roman = roman + "V"
+          n = n - 5
+        else
+          roman = roman + "I"
+          n = n - 1
+        end
+      end
+      return roman
     end
   end
 end
