a29da0d27c830b562207f3350eaf15f8ce98eeff
Jade D. Kharats <jade.kharats@gmail.com>
2017-05-21 13:50:08 +0200

    Add test convert(1) to I

--- spec/roman_spec.cr
+++ spec/roman_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe Roman do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  it "converts the number 1 to the string I" do
+    Roman.convert(1).should eq("I")
   end
 end
